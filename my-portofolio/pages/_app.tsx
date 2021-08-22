3a7e4ac88df80b33d5d8a4099fe879869fb862e5
unknown <mohamedsali602@live.com>
2021-08-22 13:37:02 +0200

    customizing navbar

--- my-portofolio/pages/_app.tsx
+++ my-portofolio/pages/_app.tsx
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { FC } from "react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import Router from "next/router";
+import NProgress from "nprogress";
+import Layout from "@/components/Layout";
+import "@/styles/index.css";
+import "@/styles/nprogress.css";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+NProgress.configure({ showSpinner: true });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+const App: FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lobster:wght@500;800;900&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Head>
+    <Layout>
+      <Component {...pageProps} />
+    
+    </Layout>
+  </>
+  
+);
+
+export default App;
--- my-portofolio/pages/index.tsx
+++ my-portofolio/pages/index.tsx
@@ -1,22 +1,49 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
-import Logo from '../components/Logo'
-import styles from '../styles/Home.module.css'
+import React, { FC } from "react";
+import { motion } from "framer-motion";
+import Header from "@/components/Header";
 
-const Home: NextPage = () => {
+const IndexPage: FC = () => {
   return (
-
-    <div>
-     <Header
+    <>
+      <Header
        title="Salbi Mohamed -Software Engineer, Creator"
        />
-     <Logo/>
-    </div>
+       
+      <div className="my-auto">
+        <section className="flex justify-between flex-wrap">
+          <motion.div
+            className="flex flex-col mb-6"
+            initial={{ x: -50, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ type: "spring", stiffness: 30 }}
+          >
+            <div className="mt-96">
+              <h4 className="mb-4 font-light tracking-widest">
+                Want to Network?
+              </h4>
+              <a
+                href="mailto:mohamedsali602@live.com"
+                target="_blank"
+                rel="noreferrer"
+                className="px-7 mb-2 border-4 rounded py-2 border-black bg-white hover:bg-gray-200 uppercase font-bold focus:outline-none"
+              >
+                Stay in touch
+              </a>
+            </div>
+          </motion.div>
+          {/* <motion.img
+            initial={{ x: 50, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ type: "spring", stiffness: 30 }}
+            whileHover={{ rotateZ: 3 }}
+            // src={require("@/assets/doodles/hero.png")}
+            // alt="hero"
+            className="w-4/6 sm:w-auto xl:w-5/12 mb-4 sm:mb-0 mx-auto lg:mx-0"
+          /> */}
+        </section>
+      </div>
+    </>
+  );
+};
 
-
-  )
-}
-
-export default Home
+export default IndexPage;
